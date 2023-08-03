d10c4a459bc9018733eec9d07a4e5d764ccafb5d
HanHoing <hhy6789@gmail.com>
2023-08-03 22:43:12 +0900

    section1 https://www.notion.so/hansimcom/1-5c9d916ac72c4f96b973d4b2a888e5f9?pvs=4

---
+++ hyeyoung/section2/src/chapter1.ts
@@ -0,0 +1,32 @@
+//number 
+let num1 : number = 123;
+let num2 : number = -123;
+let num3 : number = 0.123;
+let num4 : number = -0.123;
+let num5 : number = Infinity; //양의 무한대
+let num6 : number = -Infinity; //음의 무한대
+let num7 : number = NaN;
+
+//string 
+let str1 : string = "hello";
+let str2 : string = 'hello';
+let str3 : string = `hello ${num1}`;
+
+//boolean 
+let bool1 : boolean = true;
+let bool2 : boolean = false;
+
+//null
+let null1 : null = null;
+
+//undefined
+let unde1: undefined = undefined;
+
+//number타입에 임시로 null값 할당 "strictNullChecks": false
+//let numA : number = null;
+
+// 리터럴 타입 : 값 그 자체가 타입
+let numA : 10 = 10;
+//numA = 12; 불가
+let str1A : "hello" = "hello";
+let boolA : false = false;
